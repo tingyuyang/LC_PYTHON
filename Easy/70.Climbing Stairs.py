5644cee4e939fb5969beb42fdf04d6798ec9ca5e
Tingyu Yang <tingyuyang@lclark.edu>
2016-09-17 01:08:51 -0700

    Update 70.Climbing Stairs.py

--- Easy/70.Climbing Stairs.py
+++ Easy/70.Climbing Stairs.py
@@ -16,3 +16,17 @@ class Solution(object):
             return n
         else:
             return (self.climbStairs(n-1)+self.climbStairs(n-2))
+
+#ACCEPT ONE!
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        result=[0,1,2]
+    	if n<=2:
+    		return result[n]
+    	for i in range(3,n+1):
+    		result.append(result[i-1]+result[i-2])
+    	return result[n]
