6648109f998a00d2221056bf5dad713bf2763647
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-01 01:41:05 -0700

    Update 20. valid parentheses.py

--- Easy/20. valid parentheses.py
+++ Easy/20. valid parentheses.py
@@ -1,3 +1,8 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+"""
 def valid(s):
 	stack=[]
 	for ch in s:
