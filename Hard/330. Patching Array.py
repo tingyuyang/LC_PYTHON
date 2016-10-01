ec6ca3403f644d050c8147deac7aff33a603e773
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-01 01:44:16 -0700

    Update 330. Patching Array.py

--- Hard/330. Patching Array.py
+++ Hard/330. Patching Array.py
@@ -14,12 +14,7 @@ Example 2:
 nums = [1, 5, 10], n = 20
 Return 2.
 The two patches can be [2, 4].
-
-Example 3:
-nums = [1, 2, 2], n = 5
-Return 0.
 """
-
 #so basically, the individual element or the sum of those element should have result like->1,2,3,4,5,6,.....n.
 
 #complexity: O(n)
