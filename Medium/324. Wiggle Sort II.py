b74d85e6339cf7177bc6a5daeb7bc12bcabd178c
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-04 02:11:33 -0700

    Update 324. Wiggle Sort II.py

--- Medium/324. Wiggle Sort II.py
+++ Medium/324. Wiggle Sort II.py
@@ -6,7 +6,39 @@ Example:
 (1) Given nums = [1, 5, 1, 1, 6, 4], one possible answer is [1, 4, 1, 5, 1, 6]. 
 (2) Given nums = [1, 3, 2, 2, 3, 1], one possible answer is [2, 3, 1, 3, 1, 2].
 """
-#kinda confused, coz they did not tell me whats the specific order they want?....
+       
+#final solution
+"""
+after sort [1,2,3|4,5,6] <-slice into half, put into resultList from end for both half
+after wiggle [3,6,2,5,1,4]
+
+Also, need to consider the possiblity when --->len(result)=7,9,11(ODD number)
+"""
+class Solution(object):
+    def wiggleSort(self, result):
+        """
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        result.sort()
+        sortList=result[:]
+        i=0
+        if len(result)%2==0:
+        	j=len(result)//2-1
+        else:
+        	j=len(result)//2
+        last=len(result)-1
+        while (i<len(result)):
+        	if i%2==1:
+        		result[i]=sortList[last]
+        		last-=1
+        	else:
+        		result[i]=sortList[j]
+        		j-=1
+        	i+=1
+        print(result)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#first solution,kinda confused, coz they did not tell me whats the specific order they want?....
 class Solution(object):
     def wiggleSort(self, result):
         """
