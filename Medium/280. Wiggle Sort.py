2ebd6be1f80934b5eda7856325025c7ea06b6585
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-05 04:28:29 -0700

    Update 280. Wiggle Sort.py

--- Medium/280. Wiggle Sort.py
+++ Medium/280. Wiggle Sort.py
@@ -20,3 +20,12 @@ def wiggle(list):
 		i+=1
 	return(result)
 print(wiggle(list))
+
+#with less space
+nums=[1,2,3,4,5]
+def wiggle(nums):
+    for i in range(1,len(nums)):
+    	if i%2==1 and nums[i]<nums[i-1] or i%2==0 and nums[i]>nums[i-1]:
+    		nums[i],nums[i-1]=nums[i-1],nums[i]
+    return nums
+print(wiggle(nums))
