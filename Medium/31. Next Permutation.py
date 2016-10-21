72492c22b704e9b2ae00eb49472320b709e3e638
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-20 21:21:09 -0700

    Update 31. Next Permutation.py

--- Medium/31. Next Permutation.py
+++ Medium/31. Next Permutation.py
@@ -1,20 +1,18 @@
 #http://bangbingsyb.blogspot.com/2014/11/leetcode-next-permutation.html
+list=[5,2,3,1] #->[5,3,1,2] is correct
 def nextPermutation(list):
 	n=len(list)-1
 	for i in range(len(list)-1,-1,-1):
-		if list[i]>list[i-1]: #NOTICE: list[i-1] is the item we want to swap!!!!
+		if list[i]>list[i-1]: 
 			break
 	else:
 		list.reverse()
 		return list
-	for j in range(i,n):
+	for j in range(n,i-1,-1):
 		if list[j]>list[i-1]:
 			list[i-1],list[j]=list[j],list[i-1]
-	#list will be [5,3,2,1] here
-	
+			break
 	i=i-1
-	#print(list[i+0+1])
-	#print(list[len(list)-0-1])
 	for j in range(0, (len(list) - i)//2):
 	    list[i+j+1], list[len(list)-j-1] = list[len(list)-j-1], list[i+j+1]
 	print(list)
