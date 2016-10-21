ecc22d62399850890a9b5cdcdd70563f69f38b99
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-20 21:29:41 -0700

    Update 31. Next Permutation.py

--- Medium/31. Next Permutation.py
+++ Medium/31. Next Permutation.py
@@ -1,5 +1,13 @@
 #http://bangbingsyb.blogspot.com/2014/11/leetcode-next-permutation.html
 #kinda right thought, but not correct exactly!!!
+"""
+1. find from the list[end] to list[0] order 
+	i. find if any list[i]>list[i-1]    (e.g. 523,2>3)
+	   break the for loop
+	ii. if not, it means the list already dictionary end.
+	    reverse the list
+2. 
+"""
 list=[5,2,3,1] #->[5,3,1,2] is correct
 def nextPermutation(list):
 	n=len(list)-1
