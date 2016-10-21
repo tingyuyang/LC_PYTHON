d9349749995d60aa2d7133f3ccbf0e1906e4f8ec
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-20 21:21:52 -0700

    Update 31. Next Permutation.py

--- Medium/31. Next Permutation.py
+++ Medium/31. Next Permutation.py
@@ -1,4 +1,5 @@
 #http://bangbingsyb.blogspot.com/2014/11/leetcode-next-permutation.html
+#kinda right thought, but not correct exactly!!!
 list=[5,2,3,1] #->[5,3,1,2] is correct
 def nextPermutation(list):
 	n=len(list)-1
