3aa74f24233fe79feb5e49079a520984bbc3c884
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-22 21:21:02 -0700

    Create Missing Word.py

---
+++ Missing Word.py
@@ -0,0 +1,22 @@
+"""
+s="i am using books"
+t="am books"
+
+print out what is missing b/w s and t:
+i using 
+"""
+s="i am using books"
+t="am books"
+def missingWord(s,t):
+	i=0
+	j=0
+	result=""
+	while i <len(s):
+		if s[i]!=t[j]:
+			result+=s[i]
+		else:
+			j=j+1
+		i+=1
+	return result
+print(missingWord(s,t))
+#i using 
