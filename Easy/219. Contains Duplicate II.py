33abb0695699c12e29cc81757a319a2d49bd536b
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-31 22:39:16 -0700

    Update 219. Contains Duplicate II.py

--- Easy/219. Contains Duplicate II.py
+++ Easy/219. Contains Duplicate II.py
@@ -5,8 +5,21 @@ such that nums[i] = nums[j] and the difference between i and j is at most k.
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
     	vis = {}
-    	for i, num in enumerate(nums):#n would be the number, i would be the index
+    	for i, num in enumerate(nums):#num would be the number, i would be the index
     		if num in vis and i-vis[num]<=k:
     			return True
     		vis[num] = i
     	return False
+"""
+e.g. 
+nums=[1,2,1,4,5]
+1 0
+2 1
+1 2
+4 3
+5 4
+{1: 2, 2: 1, 4: 3, 5: 4}
+
+so "num would be the number, i would be the index"
+but in hash table is totally opposite
+"""
