a31ba342002f272e570aad7a67e6a68610c3c36c
Tingyu Yang <tingyuyang@lclark.edu>
2016-10-31 19:34:56 -0700

    Update 198. House Robber.py

--- Easy/198. House Robber.py
+++ Easy/198. House Robber.py
@@ -1,5 +1,11 @@
 """
-use dp(dynamic programming) here!
+use DP(dynamic programming) here!
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and 
+it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
 reference w/ good explanation:
 http://www.qiujiawei.com/leetcode-problem-198/
 """
