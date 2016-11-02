b62351510c746a6da7d15c4f4ce64a3cb34f9290
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-01 21:59:03 -0700

    Create 242. Valid Anagram.py

---
+++ Easy/242. Valid Anagram.py
@@ -0,0 +1,18 @@
+"""
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+"""
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s)!=len(t):
+            return False
+        a=sorted(s)
+        b=sorted(t)
+        for i in range(len(a)):
+            if a[i]!=b[i]:
+                return False
+        return True
+            
