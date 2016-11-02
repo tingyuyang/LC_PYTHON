23bed2396cdebdbaa2f9334c9622755a1d3960ef
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-01 23:12:52 -0700

    Create 1. Two Sum.py

---
+++ Easy/1. Two Sum.py
@@ -0,0 +1,19 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        vis={}
+    	for i, n in enumerate(nums):
+    		rest =target-n
+    		if rest in vis:
+    			return vis[rest],i #因为题只要求2个数，所以简单很多！
+    		vis[n]=i
